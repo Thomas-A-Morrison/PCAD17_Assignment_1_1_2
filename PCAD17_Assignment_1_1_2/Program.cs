53dc9a05188143fe5114f280647992a87da1d43c
Thoma <Thoma@LAPTOP-LU9AH37M>
2025-04-07 17:35:07 -0400

    Add project files.

---
+++ PCAD17_Assignment_1_1_2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Program:    PCAD_Project_1.1.2
+// Date:       7 APR 2025
+// Programmer: Thomas A. Morrison
+
+using System;
+using System.Globalization;
+namespace PCAD_Project_1_1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter the first number: ");
+            double firstNumber = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Please enter the second number: ");
+            double secondNumber = Convert.ToDouble(Console.ReadLine());
+
+            double sumNumber = firstNumber + secondNumber;
+
+            Console.WriteLine($"The sum of {firstNumber} and {secondNumber} is {sumNumber}.");
+        }
+    }
+}
